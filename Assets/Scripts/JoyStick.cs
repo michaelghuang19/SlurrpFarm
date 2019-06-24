7c00e0812d424e315963aa5f92fff5aa19bf05fd
Jimmy Guo <jimmyg8@cs.washington.edu>
2019-06-23 21:33:38 -0700

    Finished joystick.cs

--- Assets/Scripts/JoyStick.cs
+++ Assets/Scripts/JoyStick.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class JoyStick : MonoBehaviour
 {
+    public Transform player;
+    public float speed = 7.0f;
+    private bool touchStart = false;
+    private Vector2 pointA;
+    private Vector2 pointB;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,34 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+
+
+        }
+        if(Input.GetMouseButton(0))
+        {
+            touchStart = true;
+            pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+        }
+        else
+        {
+            touchStart = false;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if(touchStart)
+        {
+            Vector2 offset = pointB - pointA;
+            Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
+            moveCharacter(direction);
+        }
+    }
+    void moveCharacter(Vector2 direction)
+    {
+        player.Translate(direction * speed * Time.deltaTime);
+
     }
 }
