5207d5e81717972401357e915ffaf5eb57060cab
NeilKagalwala <neil.kagalwala@gmail.com>
2019-06-25 11:25:15 -0700

    Auto create cards with correct background and orientation

---
+++ Assets/Scripts/MG_Memory/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite bgImage;
+
+    public List<Button> btns = new List<Button>();
+    
+    void Start()
+    {
+        GetButtons();
+    }
+
+    void GetButtons()
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("MemoryButton");
+
+        for (int i = 0; i < objects.Length; i++)
+        {
+            btns.Add(objects[i].GetComponent<Button>());
+            btns[i].image.sprite = bgImage;
+        }
+    }
+}
