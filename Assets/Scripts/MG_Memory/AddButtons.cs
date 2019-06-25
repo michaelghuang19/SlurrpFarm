e8fde948debc4ef3c8bd23267cdb550f1d6d0527
NeilKagalwala <neil.kagalwala@gmail.com>
2019-06-24 22:58:08 -0700

    Created new button prefab, programmatically generate buttons in a grid for memory minigame

---
+++ Assets/Scripts/MG_Memory/AddButtons.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddButtons : MonoBehaviour
+{
+    [SerializeField]
+    private Transform puzzleField;
+
+    [SerializeField]
+    private GameObject btnField;
+
+    void Awake()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            GameObject button = Instantiate(btnField);
+            button.name = "memoryBtn" + i;
+            //Second param of SetParent is worldPositionStays, meaning that if it is true,
+            //the item will stay in the same place it currently is, even after being assigned to a parent
+            button.transform.SetParent(puzzleField, false);
+        }
+    }
+}
