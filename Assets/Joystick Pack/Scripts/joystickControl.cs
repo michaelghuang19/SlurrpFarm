5ff3b3249055f8dd3d6c2c2cdc300996af041fc5
Jimmy Guo <jimmyg8@cs.washington.edu>
2019-06-26 03:38:16 -0700

    Finished joystick

---
+++ Assets/Joystick Pack/Scripts/joystickControl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class joystickControl : MonoBehaviour
+{
+    public float moveSpeed = 100f;
+    public JoystickFunction jsMovement;
+    public Image joystick;
+
+    private Vector3 direction;
+    private float xMin, xMax, yMin, yMax;
+
+    void Update()
+    {
+
+        direction = jsMovement.InputDirection; //InputDirection can be used as per the need of your project
+
+        if (direction.magnitude != 0)
+        {
+
+            transform.position += direction * moveSpeed;
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, xMin, xMax), Mathf.Clamp(transform.position.y, yMin, yMax), 0f);//to restric movement of player
+
+
+        }
+    }
+
+    void Start()
+    {
+
+        //Initialization of boundaries
+        xMax = Screen.width - 50; // I used 50 because the size of player is 100*100
+        xMin = 50;
+        yMax = Screen.height - 50;
+        yMin = 50;
+    }
+}
---
+++ Assets/Scripts/JoystickTest.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoystickTest : MonoBehaviour
+{
+    public Transform player;
+    public float speed = 5.0f;
+    private bool touchStart = false;
+    private Vector2 pointA;
+    private Vector2 pointB;
+
+    public Transform circle;
+    public Transform outerCircle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(EventSystem.current.IsPointerOverGameObject())
+            {
+                pointA = circle.position;
+                pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+                circle.GetComponent<SpriteRenderer>().enabled = true;
+                outerCircle.GetComponent<SpriteRenderer>().enabled = true;
+            }
+   
+            
+            
+        }
+        if (Input.GetMouseButton(0))
+        {
+            if (EventSystem.current.IsPointerOverGameObject())
+            {
+                touchStart = true;
+                pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+            }
+            
+        }
+        else
+        {
+            touchStart = false;
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        if (touchStart)
+        {
+            Vector2 offset = pointB - pointA;
+            Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
+            moveCharacter(direction);
+
+            circle.transform.position = new Vector2(pointA.x + direction.x *2, pointA.y + direction.y*2);
+        }
+        else
+        {
+            circle.GetComponent<SpriteRenderer>().enabled = false;
+            outerCircle.GetComponent<SpriteRenderer>().enabled = false;
+
+        }
+
+    }
+    void moveCharacter(Vector2 direction)
+    {
+        player.Translate(direction * speed * Time.deltaTime);
+    }
+
+}
