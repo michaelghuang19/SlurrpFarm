47918afbda07425923a0d4db5a8ac5ea4bcdd514
NeilKagalwala <neil.kagalwala@gmail.com>
2019-06-27 16:40:16 -0700

    Cookie catcher movement enabled

---
+++ Assets/Scripts/MG_Duck/CatcherMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatcherMovement : MonoBehaviour
+{
+    private Vector3 touchPos;
+    private Rigidbody2D rb;
+    private Vector3 direction;
+    private float moveSpeed = 20f;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+            touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+            touchPos.z = 0;
+            direction = (touchPos - transform.position);
+            rb.velocity = new Vector2(0, direction.y) * moveSpeed;
+
+            if(touch.phase == TouchPhase.Ended) {
+                rb.velocity = Vector2.zero;
+            }
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/MG_Duck/DuckGameController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckGameController : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite item;
+    [SerializeField]
+    private GameObject character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
