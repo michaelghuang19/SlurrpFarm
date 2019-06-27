2207a8bb16ff1c7602f623646b514dabcce1af64
Jimmy Guo <jimmyg8@cs.washington.edu>
2019-06-26 17:09:12 -0700

    Finished basic structure of open world

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Joystick joystick;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float horizontalMove = joystick.Horizontal;
+        float verticalMove = joystick.Vertical;
+        Vector2 position = transform.position;
+        position.x = position.x + 30f * horizontalMove * Time.deltaTime;
+        position.y = position.y + 30f * verticalMove * Time.deltaTime;
+        transform.position = position;
+    }
+
+
+}
