8b713582f1e68c09947fcdfe79e035cacccc3bbd
Michael Huang <mhuang19@cs.washington.edu>
2019-06-30 18:27:12 -0700

    added base for character creation

---
+++ Assets/Resources/CharacterSelect/rightcolor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rightarrowscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ChangeItemLeft()
+    {
+        Debug.Log("Left works");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
