370a11f3df0834f5e34c82f120a36af6468d14d5
Jimmy Guo <jimmyg8@cs.washington.edu>
2019-06-30 18:24:39 -0700

    Made OpenWorld better

---
+++ Assets/Scripts/OpenToSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenToSettings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void OpenSettings()
+    {
+        SceneManager.LoadScene(4);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,15 +9,16 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        float lockPos = 0;
         float horizontalMove = joystick.Horizontal;
         float verticalMove = joystick.Vertical;
         Vector2 position = transform.position;
-        position.x = position.x + 100f * horizontalMove * Time.deltaTime;
-        position.y = position.y + 100f * verticalMove * Time.deltaTime;
+        position.x = position.x + 75f * horizontalMove * Time.deltaTime;
+        position.y = position.y + 75f * verticalMove * Time.deltaTime;
         if(position.x < 980.5 && position.x > 400 && position.y > 120 && position.y < 398.6)
         {
             transform.position = position;
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, lockPos, lockPos);
         }
     }
 
