350a9d2b9cdf1b8509b6c5a68f99c4ff56107b15
NeilKagalwala <neil.kagalwala@gmail.com>
2019-07-02 12:47:00 -0700

    Added EXP to MG_Memory

--- Assets/Scripts/MG_Memory/GameController.cs
+++ Assets/Scripts/MG_Memory/GameController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -23,8 +24,10 @@ public class GameController : MonoBehaviour
     private int gameGuesses;
 
     private int firstGuessIndex, secondGuessIndex;
-
     private string firstGuessCard, secondGuessCard;
+
+    private bool gameWon = false;
+    private int GameID = 2;
 
 
     
@@ -131,16 +134,88 @@ public class GameController : MonoBehaviour
         countCorrectGuesses++;
 
         if (countCorrectGuesses == gameGuesses) {
-            
+            SetExp();
             SceneManager.LoadScene(1);
             Debug.Log("You have finished the game!");
+        }
+    }
+
+    void SetExp() {
+        CheckDefaultPrefs();
+        
+        int PlayerLevel = PlayerPrefs.GetInt("PlayerLevel");
+        int CurEXP = PlayerPrefs.GetInt("CurExp");
+        int PrevGameCount = PlayerPrefs.GetInt("PrevGameCount");
+        int PrevGame = PlayerPrefs.GetInt("PrevGame");
+
+        bool levelChanged = false;
+        bool gameChanged = false;
+
+        int exp = 0;
+
+        if (gameWon) {
+            exp+=100;
+        } else {
+            exp+=34;
+        }
+
+        if (PrevGame != GameID) {
+            PrevGameCount = 0;
+            gameChanged = true;
+        } else {
+            PrevGameCount +=1;
+        }
+        exp = (int) (exp * (Math.Pow(0.8, PrevGameCount - 1)));
+        CurEXP += exp;
+        if (PlayerLevel == 1) {
+            if (CurEXP >= 100) {
+                PlayerLevel += 1;
+                CurEXP -= 100;
+                levelChanged = true;
+            }
+        } else if (PlayerLevel == 2) {
+            if (CurEXP >= 200) {
+                PlayerLevel += 1;
+                CurEXP -= 200;
+                levelChanged = true;
+            }
+        } else {
+            if (CurEXP >= 300) {
+                PlayerLevel += 1;
+                CurEXP -= 300;
+                levelChanged = true;
+            }
+        }
+
+        PlayerPrefs.SetInt("CurEXP", CurEXP);
+        PlayerPrefs.SetInt("PrevGameCount", PrevGameCount);
+        if (levelChanged) {
+            PlayerPrefs.SetInt("PlayerLevel", PlayerLevel);
+        }
+        if (gameChanged) {
+            PlayerPrefs.SetInt("PrevGame", GameID);
+        }
+    }
+
+    void CheckDefaultPrefs() {
+        if (!PlayerPrefs.HasKey("PlayerLevel")) {
+            PlayerPrefs.SetInt("PlayerLevel", 1);
+        }
+        if (!PlayerPrefs.HasKey("CurEXP")) {
+            PlayerPrefs.SetInt("CurEXP", 0);
+        }
+        if (!PlayerPrefs.HasKey("PrevGame")) {
+            PlayerPrefs.SetInt("PrevGame", 0);
+        }
+        if (!PlayerPrefs.HasKey("PrevGameCount")) {
+            PlayerPrefs.SetInt("PrevGameCount", 0);
         }
     }
 
     void Shuffle(List<Sprite> list) {
         for (int i = 0; i < list.Count; i++) {
             Sprite temp = list[i];
-            int randomIndex = Random.Range(i, list.Count);
+            int randomIndex = UnityEngine.Random.Range(i, list.Count);
             list[i] = list[randomIndex];
             list[randomIndex] = temp;
         }
