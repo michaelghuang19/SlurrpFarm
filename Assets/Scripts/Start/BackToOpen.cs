fb7bc5a73f0bd17f98b3ef5382d8a64826c2061e
NeilKagalwala <neil.kagalwala@gmail.com>
2019-07-10 14:52:26 -0700

    Reward screen enabled

---
+++ Assets/Scripts/Start/BackToOpen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackToOpen : MonoBehaviour
+{
+    [SerializeField]
+    Canvas UICanvas;
+    [SerializeField]
+    Canvas DiscountCanvas;
+    // Start is called before the first frame update
+
+    public void GoBack() 
+    {
+        UICanvas.gameObject.SetActive(true);
+        DiscountCanvas.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Start/GameControllerOpen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerOpen : MonoBehaviour
+{
+    [SerializeField]
+    Canvas UICanvas;
+    [SerializeField]
+    Canvas DiscountCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool levelledUp = Convert.ToBoolean(PlayerPrefs.GetInt("LevelChanged"));
+        if (levelledUp) {
+            UICanvas.gameObject.SetActive(false);
+            DiscountCanvas.gameObject.SetActive(true);
+        } else {
+            UICanvas.gameObject.SetActive(true);
+            DiscountCanvas.gameObject.SetActive(false);
+        }
+    }
+
+}
