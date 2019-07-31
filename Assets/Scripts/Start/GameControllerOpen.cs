753df4a05ca320d1092621049e17c7e00da14a99
NeilKagalwala <neil.kagalwala@gmail.com>
2019-07-30 22:19:59 -0700

    Possible to remotely disable discount codes

--- Assets/Scripts/Start/GameControllerOpen.cs
+++ Assets/Scripts/Start/GameControllerOpen.cs
@@ -1,20 +1,16 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
 using Amazon;
 using Amazon.CognitoIdentity;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
 using Amazon.DynamoDBv2.DocumentModel;
 using Amazon.DynamoDBv2.Model;
+using UnityEngine;
+using UnityEngine.UI;
 
-
-
-
-public class GameControllerOpen : MonoBehaviour
-{
+public class GameControllerOpen : MonoBehaviour {
     [SerializeField]
     Canvas UICanvas;
     [SerializeField]
@@ -24,82 +20,71 @@ public class GameControllerOpen : MonoBehaviour
     private AmazonDynamoDBClient client;
     private CognitoAWSCredentials credentials;
     private DynamoDBContext Context;
+    private bool codeAvail;
 
-
-    void Awake() {
-        UnityInitializer.AttachToGameObject(this.gameObject);
+    void Awake () {
+        UnityInitializer.AttachToGameObject (this.gameObject);
         AWSConfigs.HttpClient = AWSConfigs.HttpClientOption.UnityWebRequest;
     }
     // Start is called before the first frame update
-    void Start()
-    {   
-        
+    void Start () {
 
         credentials = new CognitoAWSCredentials (
             "us-west-2:a7272201-d02b-449c-9dca-abeaeb7a5e4a", // Identity pool ID
             RegionEndpoint.USWest2 // Region
         );
-        client = new AmazonDynamoDBClient(credentials, RegionEndpoint.USWest2);
-        Context = new DynamoDBContext(client);
+        client = new AmazonDynamoDBClient (credentials, RegionEndpoint.USWest2);
+        Context = new DynamoDBContext (client);
 
-        genCode();
+        genCode ();
 
-        
-
-
-        settingsCanvas.gameObject.SetActive(false);
-        bool levelledUp = Convert.ToBoolean(PlayerPrefs.GetInt("LevelChanged"));
+        settingsCanvas.gameObject.SetActive (false);
+        bool levelledUp = Convert.ToBoolean (PlayerPrefs.GetInt ("LevelChanged"));
         if (levelledUp) {
-            Text codeText = GameObject.Find("/PopupCanvas/PopupBackground/Code").GetComponent<Text>();
-            codeText.text = genCode();
-            UICanvas.gameObject.SetActive(false);
-            DiscountCanvas.gameObject.SetActive(true);
+            Text codeText = GameObject.Find ("/PopupCanvas/PopupBackground/Code").GetComponent<Text> ();
+            codeText.text = genCode ();
+            UICanvas.gameObject.SetActive (false);
+            DiscountCanvas.gameObject.SetActive (true);
         } else {
-            UICanvas.gameObject.SetActive(true);
-            DiscountCanvas.gameObject.SetActive(false);
+            UICanvas.gameObject.SetActive (true);
+            DiscountCanvas.gameObject.SetActive (false);
         }
     }
 
-    private String genCode() {
+    private String genCode () {
         var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         var stringChars = new char[8];
-        var random = new System.Random();
+        var random = new System.Random ();
 
-        for (int i = 0; i < stringChars.Length; i++)
-        {
-            stringChars[i] = chars[random.Next(chars.Length)];
+        for (int i = 0; i < stringChars.Length; i++) {
+            stringChars[i] = chars[random.Next (chars.Length)];
         }
 
-        var finalString = new String(stringChars);
-        CodeClass code = new CodeClass{
+        var finalString = new String (stringChars);
+        CodeClass code = new CodeClass {
             Code = finalString,
             Uses = 1
         };
-        Context.SaveAsync(code, (result)=>{
-            if(result.Exception == null) {
-                Debug.Log("Save worked");
+
+        Context.SaveAsync (code, (result) => {
+            if (result.Exception == null) {
+                Debug.Log ("Save worked");
+                codeAvail = true;
             } else {
-                Debug.Log(result.Exception);
+                Debug.Log (result.Exception);
             }
         });
-
-
-        return finalString;
+        if (codeAvail) { return finalString; } else { return "No Codes Available"; }
     }
-
-
-    
 
 }
 
+[DynamoDBTable ("SlurrpFarm")]
+public class CodeClass {
+    [DynamoDBHashKey] // Hash key.
+    public string Code { get; set; }
 
-[DynamoDBTable("SlurrpFarm")]
-    public class CodeClass 
-    {
-        [DynamoDBHashKey]   // Hash key.
-        public string Code { get; set; }
-        [DynamoDBProperty]
-        public int Uses {get; set;}
+    [DynamoDBProperty]
+    public int Uses { get; set; }
 
-
-    }
+}
