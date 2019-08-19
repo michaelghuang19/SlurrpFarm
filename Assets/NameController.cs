8c7f4004bc7ed8eb7279be9f72c96da50d2c907b
Michael Huang <mhuang19@cs.washington.edu>
2019-08-19 14:53:11 -0700

    fixed scaling for matching and name for open world

---
+++ Assets/NameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text username;
+
+    void Start()
+    {
+        PlayerPrefs.SetString("Name", "Patrick");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        username.text = PlayerPrefs.GetString("Name");
+    }
+}
