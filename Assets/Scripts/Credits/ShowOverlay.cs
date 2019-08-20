f2d9d263a9e037ebbd711bfb96420a58fac91c62
NeilKagalwala <neil.kagalwala@gmail.com>
2019-08-19 19:37:25 -0700

    Credit scene

---
+++ Assets/Scripts/Credits/ShowOverlay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowOverlay : MonoBehaviour
+{
+    public Canvas overlayCanvas;
+    public Canvas creditCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        creditCanvas.gameObject.SetActive(true);
+        overlayCanvas.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void showAssetsOver() {
+        creditCanvas.gameObject.SetActive(false);
+        overlayCanvas.gameObject.SetActive(true);
+    }
+    public void hideAssetsOver() {
+        overlayCanvas.gameObject.SetActive(false);
+        creditCanvas.gameObject.SetActive(true);
+    }
+}
