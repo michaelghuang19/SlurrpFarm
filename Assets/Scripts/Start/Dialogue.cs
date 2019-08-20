c0029bdaaeae960a4c010c3dccafbc7a27d92803
Michael Huang <mhuang19@cs.washington.edu>
2019-08-19 20:31:07 -0700

    Fixed some open world text displays

--- Assets/Scripts/Start/Dialogue.cs
+++ Assets/Scripts/Start/Dialogue.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Dialogue : MonoBehaviour
 {
     public GameObject bearDialogue;
-
     public GameObject rhinoDialogue;
+    public GameObject joystick;
+    public Text bearText;
+    public Text rhinoText;
 
     // Start is called before the first frame update
     void Start()
@@ -16,29 +19,38 @@ public class Dialogue : MonoBehaviour
 
         rhinoDialogue = GameObject.Find("RhinoUI");
         rhinoDialogue.SetActive(false);
+
     }
 
     public void PlayBearDialogue()
     {
         Debug.Log("Bear Clicked");
         bearDialogue.SetActive(true);
+        joystick.SetActive(false);
     }
 
     public void StopBearDialogue()
     {
         Debug.Log("Bear Dialogue Clicked");
         bearDialogue.SetActive(false);
+        joystick.SetActive(true);
+
+        bearText.text = "Visit slurrpfarm.com for more cool products! ";
     }
 
     public void PlayRhinoDialogue()
     {
-        Debug.Log("Bear Clicked");
+        Debug.Log("Rhino Clicked");
         rhinoDialogue.SetActive(true);
+        joystick.SetActive(false);
     }
 
     public void StopRhinoDialogue()
     {
         Debug.Log("Rhino Dialogue Clicked");
         rhinoDialogue.SetActive(false);
+        joystick.SetActive(true);
+
+        rhinoText.text = "Eat healthy and you can be a superhero like me! ";
     }
 }
